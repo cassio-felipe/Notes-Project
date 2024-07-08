24f9b4961f26b4bf1ce976ca7890c1c98e5a46ee
cassiofelipe <cassiofscosta@gmail.com>
2024-07-08 07:58:27 -0300

    Adicionado prisma e metodos de create e query

--- notes-app-server/src/index.ts
+++ notes-app-server/src/index.ts
@@ -1,15 +1,44 @@
 import express from "express";
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/api/notes", async (req, res) => {
-    res.json({message: "success!"});
+    
+    
+    const notes = await prisma.note.findMany();
+
+    res.json(notes);
+});
+
+app.post("/api/notes", async (req, res) => {
+    
+    const {title, content } = req.body;
+    
+    if(!title || !content) {
+        return res
+            .status(400)
+            .send("Required fields are necessary!");
+    }
+    
+    try {
+        const note = await prisma.note.create({
+            data: {title, content}
+        });
+
+        res.json(note);
+    } catch (error) {
+        res
+            .status(500)
+            .send("Ooops somenthing went wrong");
+    }
 });
 
 app.listen(5000, () => {
-    console.log("Server running on localhost:5000")
+    console.log("Server running on localhost:50 00")
 });
